0fcc73d7ab17861ad65fa778defc735006ea02da
mani2012 <mani2012@bu.edu>
2015-12-28 00:38:03 -0500

    Adding example real data set

--- inst/example/batchqc_pipeline.R
+++ inst/example/batchqc_pipeline.R
@@ -31,6 +31,32 @@ batchQC(combat_data.matrix, batch=batch, condition=condition,
         interactive=FALSE)
 
 
+library(BatchQC)
+### Real signature dataset
+### signature data—activating different growth pathway genes (treat[,2]) 
+### in human mammary epithelial cells. 
+#signature_data <- readRDS("andrea_signature_data.rds")
+#batch_indicator <- readRDS("batch_indicator.rds")
+#save(signature_data, batch_indicator, file="example_batchqc_data.rda")
+data(example_batchqc_data)
+batch <- batch_indicator$V1
+condition <- batch_indicator$V2
+batchQC(signature_data, batch=batch, condition=condition, 
+        report_file="batchqc_signature_data_report.html", report_dir=".", report_option_binary="111111111",
+        view_report=FALSE, interactive=TRUE)
+### apply combat
+nsample <- dim(signature_data)[2]
+sample <- 1:nsample
+pdata <- data.frame(sample, batch, condition)
+modmatrix = model.matrix(~as.factor(condition), data=pdata)
+combat_data.matrix = ComBat(dat=signature_data, batch=batch, mod=modmatrix)
+
+### Rerun the BatchQC pipeline on the batch adjusted data
+batchQC(combat_data.matrix, batch=batch, condition=condition, 
+        report_file="batchqc_combat_adj_signature_data_report.html", report_dir=".", report_option_binary="110011111",
+        interactive=FALSE)
+
+
 ### Real bladderbatch dataset
 library(bladderbatch)
 data(bladderdata)
@@ -39,6 +65,14 @@ pheno <- pData(bladderEset)
 edata <- exprs(bladderEset)
 batch <- pheno$batch  ### note 5 batches, 3 covariate levels. Batch 1 contains only cancer, 2 and 3 have cancer and controls, 4 contains only biopsy, and 5 contains cancer and biopsy
 condition <- pheno$cancer
+
+#### Filtering only batch 1, 2 and 3 ####
+# index <- which((pheno$batch==1) | (pheno$batch==2) | (pheno$batch==3))
+# pheno <- pheno[index,]
+# batch <- pheno$batch
+# condition <- pheno$cancer
+# edata <- edata[,index]
+
 batchQC(edata, batch=batch, condition=condition, 
         report_file="batchqc_report.html", report_dir=".", report_option_binary="111111111",
         view_report=FALSE, interactive=TRUE)
