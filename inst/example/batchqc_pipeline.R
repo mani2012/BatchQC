070591fbd7380ca510ce42fc587e6615fe8ac7b9
mani2012 <mani2012@bu.edu>
2015-12-28 21:47:38 -0500

    Cleaning up

--- inst/example/batchqc_pipeline.R
+++ inst/example/batchqc_pipeline.R
@@ -14,7 +14,6 @@ nsample <- nbatch*ncond*npercond
 sample <- 1:nsample
 pdata <- data.frame(sample, batch, condition)
 modmatrix = model.matrix(~as.factor(condition), data=pdata)
-#pca <- batchQC_analyze(data.matrix, batch=batch, mod=modmatrix)
 
 batchQC(data.matrix, batch=batch, condition=condition, 
         report_file="batchqc_report.html", report_dir=".", report_option_binary="111111111",
@@ -25,19 +24,14 @@ batchQC(data.matrix, batch=batch, condition=condition,
 combat_data.matrix = ComBat(dat=data.matrix, batch=batch, mod=modmatrix)
 
 ### Rerun the BatchQC pipeline on the batch adjusted data
-#pca <- batchQC_analyze(combat_data.matrix, batch=batch, mod=modmatrix)
 batchQC(combat_data.matrix, batch=batch, condition=condition, 
         report_file="batchqc_combat_adj_report.html", report_dir=".", report_option_binary="110011111",
         interactive=FALSE)
 
 
-library(BatchQC)
 ### Real signature dataset
 ### signature data—activating different growth pathway genes (treat[,2]) 
 ### in human mammary epithelial cells. 
-#signature_data <- readRDS("andrea_signature_data.rds")
-#batch_indicator <- readRDS("batch_indicator.rds")
-#save(signature_data, batch_indicator, file="example_batchqc_data.rda")
 data(example_batchqc_data)
 batch <- batch_indicator$V1
 condition <- batch_indicator$V2
@@ -55,25 +49,4 @@ combat_data.matrix = ComBat(dat=signature_data, batch=batch, mod=modmatrix)
 batchQC(combat_data.matrix, batch=batch, condition=condition, 
         report_file="batchqc_combat_adj_signature_data_report.html", report_dir=".", report_option_binary="110011111",
         interactive=FALSE)
-
-
-### Real bladderbatch dataset
-library(bladderbatch)
-data(bladderdata)
-#### get annotation and data for bladder cancer data ####
-pheno <- pData(bladderEset)
-edata <- exprs(bladderEset)
-batch <- pheno$batch  ### note 5 batches, 3 covariate levels. Batch 1 contains only cancer, 2 and 3 have cancer and controls, 4 contains only biopsy, and 5 contains cancer and biopsy
-condition <- pheno$cancer
-
-#### Filtering only batch 1, 2 and 3 ####
-# index <- which((pheno$batch==1) | (pheno$batch==2) | (pheno$batch==3))
-# pheno <- pheno[index,]
-# batch <- pheno$batch
-# condition <- pheno$cancer
-# edata <- edata[,index]
-
-batchQC(edata, batch=batch, condition=condition, 
-        report_file="batchqc_report.html", report_dir=".", report_option_binary="111111111",
-        view_report=FALSE, interactive=TRUE)
 
