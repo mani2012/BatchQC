c767fead19e9c93de8a0be21f4f8daaf26ba1955
mani2012 <mani2012@bu.edu>
2016-03-02 13:07:13 -0500

    Cleaning up

--- inst/example/batchqc_pipeline.R
+++ inst/example/batchqc_pipeline.R
@@ -14,7 +14,6 @@ batchQC(data.matrix, batch=batch, condition=condition,
         report_file="batchqc_report.html", report_dir=".", report_option_binary="111111111",
         view_report=FALSE, interactive=TRUE)
 
-
 ### apply combat
 nsample <- nbatch*ncond*npercond
 sample <- 1:nsample
@@ -26,19 +25,6 @@ combat_data.matrix = ComBat(dat=data.matrix, batch=batch, mod=modmatrix)
 batchQC(combat_data.matrix, batch=batch, condition=condition, 
         report_file="batchqc_combat_adj_report.html", report_dir=".", report_option_binary="110011111",
         interactive=FALSE)
-
-### Generating Real signature dataset
-signature_data <- readRDS("andrea_signature_data.rds")
-batch_indicator <- readRDS("batch_indicator.rds")
-rn <- paste("Gene", 1:length(rownames(signature_data)), sep='')
-rownames(signature_data) <- rn
-condition <- batch_indicator$V2
-cn <- paste("Pathway", condition-1, sep='')
-changestring <- function(x)  { if(x=="Pathway0") "Control" else {x}}
-cn <- unlist(lapply(cn, changestring))
-colnames(signature_data) <- cn
-save(signature_data, batch_indicator, file="example_batchqc_data.rda")
-
 
 ### Real signature dataset
 ### signature data—activating different growth pathway genes (treat[,2]) 
