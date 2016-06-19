c8d870156ee6583b88acce20b790843f4c8b870e
Solaiappan Manimaran <manimaran_1975@hotmail.com>
2016-06-19 03:13:26 +0000

    Added protein expression data example
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BatchQC@118752 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/batchQC.R
+++ R/batchQC.R
@@ -36,6 +36,36 @@ require(graphics)
 #' @return Signature data 
 "signature_data"
 
+#' Batch and Condition indicator for protein expression data
+#'
+#' This data consists of two batches and two conditions 
+#' corresponding to case and control for the protein expression data
+#'
+#' @name protein_example_data
+#' @format A data frame with 24 rows and 4 variables:
+#' \describe{
+#'     \item{Arrayname}{Array Name}
+#'     \item{samplename}{Sample Name}
+#'     \item{Batch}{Batch Indicator}
+#'     \item{category}{Condition (Case vs Control) Indicator}
+#' }
+#' @return Protein data sample info
+"protein_sample_info"
+
+#' Protein data with 39 protein expression levels
+#'
+#' This data consists of two batches and two conditions 
+#' corresponding to case and control
+#'
+#' @name protein_example_data
+#' @format A data frame with 39 rows and 24 variables:
+#' \describe{
+#'     \item{Columns1-24}{Control and Case samples}
+#'     \item{rows1-39}{Proteins 1-39}
+#' }
+#' @return Protein data 
+"protein_data"
+
 #' @importFrom methods setOldClass
 setOldClass("prcomp")
 ################################################################################
